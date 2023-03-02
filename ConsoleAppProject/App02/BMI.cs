3825355a79e7603606713eb4132cd4b404579435
Johnathan Barker <114746425+Johnathnat@users.noreply.github.com>
2023-03-02 20:05:55 +0000

    Update BMI.cs
    I am still having issues with my Visual studio code and  the IT team still haven't fixed the issue with my account. But VSC says there is no errors with the code but i am still unable to test it.

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -1,12 +1,31 @@
-﻿namespace ConsoleAppProject.App02
+namespace ConsoleAppProject.App02
 {
     /// <summary>
     /// Please describe the main features of this App
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// John Barker version 0.1
     /// </author>
+    using System;
+
     public class BMI
     {
+        static void Main()
+        {
+            Console.WriteLine("Enter your weight in kilograms: ");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your height in meters: ");
+            double height = double.Parse(Console.ReadLine());
+
+            double bmi = CalculateBMI(weight, height);
+
+            Console.WriteLine($"Your BMI is: {bmi:F2}");
+        }
+
+        static double CalculateBMI(double weight, double height)
+        {
+            return weight / (height * height);
+        }
     }
 }
